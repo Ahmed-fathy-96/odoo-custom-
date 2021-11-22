49a15ad47c4750b3c384b699d3f9ec61d24e09de
ahmed <afathy@zadsolutions.com>
2021-11-22 18:20:12 +0200

    task 2 and report

--- purchase_request/__manifest__.py
+++ purchase_request/__manifest__.py
@@ -1,6 +1,7 @@
 {
     'name': 'Purchase Request',
     'summary': 'training model Purchase Request',
+    'category': 'Inventory/Purchase',
     'depends': [
         'purchase',
     ],
@@ -8,8 +9,11 @@
         'security/ir.model.access.csv',
         'wizards/reject_purchase_request.xml',
         'data/mail_template.xml',
+        'report/purchase_request_report_template.xml',
+        'report/purchase_request_reports.xml',
         'views/purchase_request.xml',
         'views/purchase_order.xml',
         'views/menus.xml',
+
     ],
 }
--- purchase_request/models/purchase_request.py
+++ purchase_request/models/purchase_request.py
@@ -1,12 +1,12 @@
 from odoo import api, fields, models
-from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.exceptions import UserError
 
 
 class PurchaseRequest(models.Model):
     _name = "purchase.request"
     _description = "Purchase Request Model"
 
-    name = fields.Char(string="Request name", required=True)
+    name = fields.Char(string="Request name", required=True, default='New')
     user_id = fields.Many2one('res.users', string="Requested by", required=True, default=lambda self: self.env.user)
     partner_id = fields.Many2one('res.partner', required=True, string="Vendor")
     start_date = fields.Date(default=lambda self: fields.Datetime.today())
@@ -22,102 +22,123 @@ class PurchaseRequest(models.Model):
                    ('to_be_approved', 'to be approved'),
                    ('approve', 'approve'),
                    ('reject', 'reject'),
-                   ('out_of_stock', 'out of stock'),
                    ('cancel', 'cancel'),
                    ])
+    out_of_stock = fields.Boolean(compute="_compute_out_of_stock")
+    company_id = fields.Many2one('res.company', 'Company', required=True, index=True,
+                                 default=lambda self: self.env.company.id)
 
     @api.depends('order_lines')
     def _compute_total_price(self):
         for record in self:
             record.total_price = sum([l.total for l in record.order_lines])
 
+    @api.depends('order_ids')
+    def _compute_out_of_stock(self):
+        orders_products = {}
+        for record in self:
+            if sum(record.get_stock_purchase().values()):
+                record.out_of_stock = False
+            else:
+                record.out_of_stock = True
+
+    def get_stock_purchase(self):
+        stock_units = {}
+        orders_products_quantity = {}
+
+        # quantities  of the old orders
+        old_orders = self.order_ids
+        for order in old_orders:
+            if order.state == 'purchase':
+                for product in order.order_line:
+                    pqty = product.product_qty
+                    pid = product.product_id.id
+                    orders_products_quantity[pid] = pqty if pid not in orders_products_quantity.keys() else \
+                        orders_products_quantity[pid] + pqty
+
+        # stock of units
+        order_lines = self.order_lines
+        for line in order_lines:
+            pid = line.product_id.id
+            stock_unite = line.quantity - orders_products_quantity[pid] if orders_products_quantity else line.quantity
+            stock_units[pid] = stock_unite if stock_unite > 0 else 0
+
+        return stock_units
+
     def submit_to_Approved_action(self):
-        self.write({'state': 'to_be_approved'})
+        for record in self:
+            record.write({'state': 'to_be_approved'})
 
     def cancel_action(self):
-        self.write({'state': 'cancel'})
+        for record in self:
+            record.write({'state': 'cancel'})
 
     def approve_action(self):
         template_id = self.env.ref('purchase_request.approve_purchase_email_template').id
         template = self.env['mail.template'].browse(template_id)
-        users = self.env['res.groups'].search([('category_id', '=', 19)]).users
+        category = self.env.ref('base.module_category_inventory_purchase', raise_if_not_found=False)
+        users = self.env['res.groups'].search([('category_id', '=', category.id)]).users
         for u in users:
             if u.id != self._uid:
                 template.write({'email_to': u.id})
         template.send_mail(self.id, force_send=True)
-
         self.write({'state': 'approve'})
 
     def confirm_reject(self):
-        self.write({'state': 'reject'})
+        for record in self:
+            record.write({'state': 'reject'})
 
     def reset_action(self):
-        self.write({'state': 'draft'})
+        for record in self:
+            record.write({'state': 'draft'})
 
     def create_purchase_order_action(self):
         new_order_lines = []
-        request_products = {}
-        orders_products = {}
-        all_stock = -1
-        stock_unite = 0
-
-        # get old orders
-        old_orders = self.env['purchase.order'].search([('request_id', '=', self.id), ('state', '=', 'purchase')])
-        for order in old_orders:
-            for product in order.order_line:
-                pqty = product.product_qty
-                pid = product.product_id.id
-                orders_products[pid] = pqty if pid not in orders_products.keys() else orders_products[pid] + pqty
+        stock_units = self.get_stock_purchase()
 
         for line in self.order_lines:
-            request_products[line.product_id.id] = line.quantity
-            if orders_products:
-                stock_unite = line.quantity - orders_products[line.product_id.id]
-                all_stock += stock_unite if stock_unite > 0 else 0
-                if 1 <= stock_unite :  # check if can make order with required quantity
-                    new_order_lines.append((0, 0, {
-                        'product_qty': 1,
-                        'price_unit': line.price_unit,
-                        'product_id': line.product_id.id,
-                        'price_subtotal': line.total,
-                    }))
-                elif stock_unite != 0:
-                    raise UserError(f'not available quantity of {line.product_id.display_name} only can get {stock_unite}')
-                else:
-                    raise UserError(f'{line.product_id.display_name} is out of stock')
-            else:
+            if 1 <= stock_units[line.product_id.id]:
                 new_order_lines.append((0, 0, {
                     'product_qty': 1,
                     'price_unit': line.price_unit,
                     'product_id': line.product_id.id,
                     'price_subtotal': line.total,
                 }))
+            elif stock_units[line.product_id.id] != 0:
+                raise UserError(
+                    f'the quantity  of {line.product_id.display_name}, only can get {stock_units[line.product_id.id]}')
+            else:
+                raise UserError(f'{line.product_id.display_name} is out of stock')
 
-        if all_stock == 0:
-            self.write({'state': 'out_of_stock'})
-            return False
-        else:
-            vals = {
-                'name': f'{self.name} / {len(old_orders) + 1}',
-                'partner_id': self.partner_id.id,
-                'company_id': self.env.company.id,
-                'user_id': self.user_id.id,
-                'order_line': new_order_lines,
-                'request_id': self.id
-            }
-            self.env['purchase.order'].create(vals)
-            print(".........order created ........." )
+        vals = {
+            'name': 'New',
+            'partner_id': self.partner_id.id,
+            'company_id': self.env.company.id,
+            'user_id': self.user_id.id,
+            'order_line': new_order_lines,
+            'request_id': self.id
+        }
+        if vals.get('name', 'New') == 'New':
+            vals['name'] = self.env['ir.sequence'].next_by_code('purchase.order.name.sequence') or 'New'
 
+        self.env['purchase.order'].create(vals)
 
     def orders_smart_button(self):
-         return {
+        return {
             'name': 'Orders',
             'type': 'ir.actions.act_window',
             'res_model': 'purchase.order',
-             'view_id': False,
+            'view_id': False,
             'view_mode': 'tree,form',
             'domain': [('request_id', '=', self.id)],
         }
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', 'New') == 'New':
+            vals['name'] = self.env['ir.sequence'].next_by_code('purchase.request.name.sequence') or '/'
+        return super(PurchaseRequest, self).create(vals)
+
 
 class PurchaseRequestLine(models.Model):
     _name = "purchase.request.line"
