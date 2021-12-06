de296c12f64fe50493fd98665efc446f0c166cc7
ahmed <afathy@zadsolutions.com>
2021-12-06 18:04:19 +0200

    .

--- account_reports_initial/static/src/js/account_reports_initial.js
+++ account_reports_initial/static/src/js/account_reports_initial.js
@@ -7,25 +7,6 @@ accountReportsWidget.include({
         events: _.extend({}, accountReportsWidget.prototype.events, {
             'change #initial_balance_check': '_onChangeInitialBalanceBool'
         }),
-//        start: function() {
-//            var self = this;
-////            if (self.report_model == 'account.general.ledger' || self.report_model == 'account.coa.report' )
-////                self.report_options['initial_balance'] = true;
-////            else
-////                self.report_options['initial_balance'] = false;
-//            var extra_info = this._rpc({
-//                    model: self.report_model,
-//                    method: 'get_report_informations',
-//                    args: [self.financial_id, self.report_options],
-//                    context: self.odoo_context,
-//                })
-//                .then(function(result){
-//                    return self.parse_reports_informations(result);
-//                });
-//            return Promise.all([extra_info, this._super.apply(this, arguments)]).then(function() {
-//                self.render();
-//            });
-//        },
         _onChangeInitialBalanceBool: function (event) {
             var self = this;
             self.report_options['initial_balance'] = $('#initial_balance_check').prop("checked");
